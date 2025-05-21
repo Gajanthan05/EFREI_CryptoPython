82af92469c957ee3f804861adb52067286870c80
Gajanthan05 <115551068+Gajanthan05@users.noreply.github.com>
2025-05-21 12:00:08 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
 from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
+from flask import Flask, render_template
 
-app = Flask(name)
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #Commit
+    return render_template('hello.html')  # Assurez-vous que ce fichier existe dans /templates
 
 key = Fernet.generate_key()
 f = Fernet(key)
@@ -20,5 +16,5 @@ def encryptage(valeur):
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
 
-if name == "main":
-  app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
